8ac8bfba29befafd02b57e9ad5e33e49fa2b8f70
Kyriakos Antoniadis <contact@ai-systems.today>
2024-01-23 22:51:41 +0200

    5th

--- App.js
+++ App.js
@@ -1,10 +1,17 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, View, Image } from 'react-native';
+
+import ImageViewer from './components/ImageViewer';
+
+const PlaceholderImage = require('./assets/images/background-image.png');
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app test! new app</Text>
+      <View style={styles.imageContainer}>
+        {/* <Image source={PlaceholderImage} style={styles.image} /> */}
+        <Image source={PlaceholderImage} style={PlaceholderImage} />
+      </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -15,6 +22,14 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#25292e',
     alignItems: 'center',
-    justifyContent: 'center',
+  },
+  imageContainer: {
+    flex: 1,
+    paddingTop: 58,
+  },
+  image: {
+    width: 320,
+    height: 440,
+    borderRadius: 18,
   },
 });
